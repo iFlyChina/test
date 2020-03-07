6d9149f70c9b32a2545ac694255c680a1674b2cd
Administrator <Administrator@WIN-5Q8U6V9P1P5>
2020-03-07 11:55:02 +0800

    hello java

---
+++ test/src/test/Test01.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test01 {
+	public static void main(String[] args) {
+		System.out.println("hello Java");
+		System.out.println("2020:03:07");
+		System.out.println("11:55");
+	}
+}
